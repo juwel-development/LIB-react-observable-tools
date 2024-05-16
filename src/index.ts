fbf1ce85c3ed1c1d435c93cd473864e2c87e29d3
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2024-05-16 17:37:24 +0200

    feat: Add documentation for useAction

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { useEffect, useRef } from 'react';
 import { Subject } from 'rxjs';
 
+
+/**
+ * calls the handler function when the returned subject is triggered
+ *
+ * @param {Function} handler - handler that is called when the subject is triggered
+ * @param {Array<unknown>} dependencies - dependencies of your handler function
+ * @returns {Subject} subject - subject that triggers the handler function
+ */
 export const useAction = (handler: () => void, dependencies: unknown[] = []) => {
     const subjectRef = useRef(new Subject<void>());
 

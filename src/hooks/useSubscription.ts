5a8522ffe30f31ca2f3fb35b87b7933444c1445c
Jens-Uwe Loessl <mail@jens-uwe-loessl.de>
2024-05-16 17:56:26 +0200

    feat: Hook that renders based on subscriptions

---
+++ src/hooks/useSubscription.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import type { Observable } from 'rxjs';
+
+/**
+ * Subscribes to an observable and renders the component when the observable emits a new value.
+ *
+ * @param {Observable} observable
+ * @returns The value emitted by the observable.
+ */
+export const useSubscription = <T>(observable: Observable<T>) => {
+    const [value, setValue] = useState<T | undefined>();
+
+    useEffect(() => {
+        const subscription = observable.subscribe(setValue);
+        return () => subscription.unsubscribe();
+    }, []);
+
+    return value;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,2 @@
-import { useEffect, useRef } from 'react';
-import { Subject } from 'rxjs';
-
-
-/**
- * calls the handler function when the returned subject is triggered
- *
- * @param {Function} handler - handler that is called when the subject is triggered
- * @param {Array<unknown>} dependencies - dependencies of your handler function
- * @returns {Subject} subject - subject that triggers the handler function
- */
-export const useAction = (handler: () => void, dependencies: unknown[] = []) => {
-    const subjectRef = useRef(new Subject<void>());
-
-    useEffect(() => {
-        const subscription = subjectRef.current.subscribe(handler);
-
-        return () => subscription.unsubscribe();
-    }, dependencies);
-
-    return subjectRef.current;
-};
+export { useAction } from './hooks/useAction';
+export { useSubscription } from './hooks/useSubscription';
